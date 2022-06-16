f6a627a7f96d490edc8b03f328cdd907d0fbc709
kishorevbhosale <kishorevbhosale@gmail.com>
2022-06-16 07:20:22 +0530

    ProductController class added

---
+++ src/main/java/com/hazelcast/cache/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.hazelcast.cache.controller;
+
+import com.hazelcast.cache.entities.Product;
+import com.hazelcast.cache.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product/api")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/getProductById/{id}")
+    public Product getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/saveProduct")
+    public Product saveProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/updateProduct/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product){
+        return productService.updateProduct(id, product);
+    }
+
+}
