0ea5cd68e9dbd06922dd5b85120ad98d13c79416
kishorevbhosale <kishorevbhosale@gmail.com>
2022-06-16 07:12:54 +0530

    ProductService class added

---
+++ src/main/java/com/hazelcast/cache/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.hazelcast.cache.service;
+
+import com.hazelcast.cache.entities.Product;
+import com.hazelcast.cache.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Cacheable(value = "product")
+    public List<Product> getAllProducts(){
+        try{
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        List<Product> products =  productRepository.findAll();
+        log.info("Loading data from DB!!!");
+        return products;
+    }
+
+    @Cacheable(value = "product", key = "#id")
+    public Product getProductById(Long id){
+        try{
+            Thread.sleep(5000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Item is not present in DB"));
+        log.info("Loading data from DB : "+product);
+        return product;
+    }
+
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    @CacheEvict(value = "product", key = "#id")
+    public Product updateProduct(Long id, Product newProduct){
+        Product product = getProductById(id);
+        product.setPrice(newProduct.getPrice());
+        product.setProductName(newProduct.getProductName());
+        return productRepository.save(product);
+    }
+}
