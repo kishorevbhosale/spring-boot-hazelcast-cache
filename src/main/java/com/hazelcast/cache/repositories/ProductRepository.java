25d33316f6fc6f27127417e0e0e29475d7a55c50
kishorevbhosale <kishorevbhosale@gmail.com>
2022-06-16 06:47:40 +0530

    product repository added

---
+++ src/main/java/com/hazelcast/cache/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.hazelcast.cache.repositories;
+
+import com.hazelcast.cache.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
